b8d2fb687d65e226258a89050e2e9f03bdee97b0
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2021-12-06 19:20:47 -0300

    delete css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Table from './components/Table';
 import PlanetsProvider from './context/PlanetsProvider';
 import FilterPlanet from './components/FilterPlanet';
