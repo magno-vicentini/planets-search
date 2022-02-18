2fcef7a9e757be0343a62ff6d3b6bdade145db87
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-02-17 22:27:37 -0300

    Updates

---
+++ precache-manifest.3703fb3040d0d6ec2632550812b4307b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c13aa2e5b7a93e15923e33a3ea766d",
+    "url": "/planets-search/index.html"
+  },
+  {
+    "revision": "196959b392c84564bb53",
+    "url": "/planets-search/static/css/main.0451aaef.chunk.css"
+  },
+  {
+    "revision": "ef7ffe85f2adc71d93b8",
+    "url": "/planets-search/static/js/2.0c769cc5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/planets-search/static/js/2.0c769cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "196959b392c84564bb53",
+    "url": "/planets-search/static/js/main.aa209d29.chunk.js"
+  },
+  {
+    "revision": "0ae763cd9413fd4ef9f3",
+    "url": "/planets-search/static/js/runtime-main.a4971208.js"
+  },
+  {
+    "revision": "c14a7b408e7c3f6ac1fa523332bf15e9",
+    "url": "/planets-search/static/media/planets-background.c14a7b40.jpeg"
+  }
+]);
